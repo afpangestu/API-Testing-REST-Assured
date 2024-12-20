5b3021e86617145896f730970714a7bb9f6974fa
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-20 18:46:57 +0700

    add post, get, delete implementing with data driven test

--- src/test/java/tests/ddt/DDTest.java
+++ src/test/java/tests/ddt/DDTest.java
@@ -8,7 +8,7 @@ import pojo.User;
 import utility.DataProviders;
 
 public class DDTest {
-    @Test(priority = 1, dataProvider = "Data", dataProviderClass = DataProviders.class)
+    @Test(priority = 1, dataProvider = "all_data", dataProviderClass = DataProviders.class)
     public void createUserTest(String uname, String fname, String lname, String mairu, String pwd, String phnmber) {
         User user = new User();
         user.setFirstName(fname);
@@ -21,5 +21,24 @@ public class DDTest {
         Response response = UserEndPoints.createUser(user);
         response.then().log().body();
         Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println("CREATE USER SUCCESS");
+        System.out.println("=================================");}
+
+    @Test(priority = 2, dataProvider = "by_user_n_pass", dataProviderClass = DataProviders.class)
+    public void loginUserTest(String uname, String pwd) {
+        Response response = UserEndPoints.loginUser(uname, pwd);
+        response.then().log().body();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println("LOGIN USER SUCCESS");
+        System.out.println("=================================");
+    }
+
+    @Test(priority = 3, dataProvider = "by_username", dataProviderClass = DataProviders.class)
+    public void deleteUserTest(String uname) {
+        Response response = UserEndPoints.deleteUser(uname);
+        response.then().log().body();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        System.out.println("DELETE USER SUCCESS");
+        System.out.println("=================================");
     }
 }
