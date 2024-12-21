f4c89db6a0f23b382eef903c4b8ddae92d4120b1
Aji Fauzi P <ajifauzi949@gmail.com>
2024-12-21 17:49:27 +0700

    update method to get endpoints url

--- src/test/java/endpoints/UserEndPoints.java
+++ src/test/java/endpoints/UserEndPoints.java
@@ -3,51 +3,63 @@ package endpoints;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import pojo.User;
-import routes.Routes;
+
+import java.util.ResourceBundle;
+
 import static io.restassured.RestAssured.given;
 
 public class UserEndPoints {
+    // get value from properties file
+    static ResourceBundle getValue() {
+        return ResourceBundle.getBundle("routes");
+    }
+
     public static Response createUser(User payload) {
+        String userCreateUrl = getValue().getString("userCreatePostUrl");
         return given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(payload)
                 .when()
-                .post(Routes.userCreatePostUrl);
+                .post(userCreateUrl);
     }
 
     public static Response loginUser(String username, String password) {
+        String userLoginUrl = getValue().getString("userLoginGetUrl");
         return given()
                 .header("Content-Type","application/json; charset=utf-8")
                 .queryParams(username, password)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .when()
-                .get(Routes.userLoginGetUrl);
+                .get(userLoginUrl);
     }
 
     public static Response logoutUser() {
+        String userLogoutUrl = getValue().getString("userLogoutGetUrl");
         return given()
-                .get(Routes.userLogoutGetUrl);
+                .get(userLogoutUrl);
     }
 
     public static Response updateUser(String username, User payload) {
+        String userUpdateUrl = getValue().getString("userUpdatePutUrl");
         return given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .pathParam("username", username)
                 .body(payload)
                 .when()
-                .put(Routes.userUpdatePutUrl);
+                .put(userUpdateUrl);
     }
 
     public static Response deleteUser(String username) {
+        String userDeleteUrl = getValue().getString("userDeleteUrl");
         return given()
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .pathParam("username", username)
                 .when()
-                .delete(Routes.userDeleteUrl);
+                .delete(userDeleteUrl);
     }
 
 }
